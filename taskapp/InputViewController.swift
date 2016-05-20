9e395a178437afb9633903f3165dcc0aa1aab476
江端正道 <ebata@me.com>
2016-05-21 00:07:49 +0900

    20160521

--- taskapp/InputViewController.swift
+++ taskapp/InputViewController.swift
@@ -10,53 +10,56 @@ import UIKit
 import RealmSwift
 
 class InputViewController: UIViewController {
-
-   
+    
     @IBOutlet weak var titleTextField: UITextField!
-    @IBOutlet weak var contentsTextView: UITextView!    
-    @IBOutlet weak var dataPicker: UIDatePicker!
+    @IBOutlet weak var contentsTextView: UITextView!
+    @IBOutlet weak var datePicker: UIDatePicker!
     
     let realm = try! Realm()
     var task:Task!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // 背景をタップしたらdismissKeyboardメソッドを呼ぶように設定する
         let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target:self, action:#selector(InputViewController.dismissKeyboard))
         self.view.addGestureRecognizer(tapGesture)
         
         titleTextField.text = task.title
         contentsTextView.text = task.contents
-        dataPicker.date = task.date
+        datePicker.date = task.date
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     override func viewWillDisappear(animated: Bool) {
         try! realm.write {
             self.task.title = self.titleTextField.text!
             self.task.contents = self.contentsTextView.text
-            self.task.date = self.dataPicker.date
+            self.task.date = self.datePicker.date
             self.realm.add(self.task, update: true)
         }
+        
+        setNotification(task)
         
         super.viewWillDisappear(animated)
     }
     
-    
-    func dismissKeyboard() {
+    func dismissKeyboard(){
+        // キーボードを閉じる
         view.endEditing(true)
     }
     
     func setNotification(task: Task) {
+        
+        // すでに同じタスクが登録されていたらキャンセルする
         for notification in UIApplication.sharedApplication().scheduledLocalNotifications! {
             if notification.userInfo!["id"] as! Int == task.id {
                 UIApplication.sharedApplication().cancelLocalNotification(notification)
-                break
+                break   // breakに来るとforループから抜け出せる
             }
         }
         
@@ -66,7 +69,7 @@ class InputViewController: UIViewController {
         notification.timeZone = NSTimeZone.defaultTimeZone()
         notification.alertBody = "\(task.title)"
         notification.soundName = UILocalNotificationDefaultSoundName
-        notification.userInfo = ["id": task.id]
+        notification.userInfo = ["id":task.id]
         UIApplication.sharedApplication().scheduleLocalNotification(notification)
         
     }
--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     @IBOutlet weak var tableView: UITableView!
     
     let realm = try! Realm()
-    let taskArray = try! Realm().objects(Task).sorted("data", ascending: false)
+    let taskArray = try! Realm().objects(Task).sorted("date", ascending: false)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,13 +26,18 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: UITableViewDataSourceプロトコルのメソッド
+    // データの数（＝セルの数）を返すメソッド
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return taskArray.count
+        return taskArray.count  // ←追加する
     }
     
+    // 各セルの内容を返すメソッド
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // 再利用可能な cell を得る
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
         
+        // Cellに値を設定する.
         let task = taskArray[indexPath.row]
         cell.textLabel?.text = task.title
         
@@ -45,24 +50,41 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return cell
     }
 
+    // MARK: UITableViewDelegateプロトコルのメソッド
+    // 各セルを選択した時に実行されるメソッド
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        performSegueWithIdentifier("cellSegue", sender: nil)
+        performSegueWithIdentifier("cellSegue",sender: nil)
     }
 
-    func tableView(tableView: UITableView, editingStyleForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCellEditingStyle {
+    // セルが削除が可能なことを伝えるメソッド
+    func tableView(tableView: UITableView, editingStyleForRowAtIndexPath indexPath: NSIndexPath)-> UITableViewCellEditingStyle {
         return UITableViewCellEditingStyle.Delete
     }
     
+    // Delete ボタンが押された時に呼ばれるメソッド
     func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == UITableViewCellEditingStyle.Delete {
+            
+            // ローカル通知をキャンセルする
+            let task = taskArray[indexPath.row]
+            
+            for notification in UIApplication.sharedApplication().scheduledLocalNotifications! {
+                if notification.userInfo!["id"] as! Int == task.id {
+                    UIApplication.sharedApplication().cancelLocalNotification(notification)
+                    break
+                }
+            }
+            
+            // データベースから削除する
             try! realm.write {
                 self.realm.delete(self.taskArray[indexPath.row])
                 tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Fade)
             }
         }
     }
-
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    
+    // segue で画面遷移するに呼ばれる
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?){
         let inputViewController:InputViewController = segue.destinationViewController as! InputViewController
         
         if segue.identifier == "cellSegue" {
@@ -80,9 +102,11 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         }
     }
     
+    // 入力画面から戻ってきた時に TableView を更新させる
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         tableView.reloadData()
     }
+
 }
 
